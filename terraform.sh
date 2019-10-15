f3480f9be4a3c5ed5daa65537b8fde4b8bf15029
Akhil Nair <akhil.nair@revolut.com>
2019-10-15 11:35:46 +0100

    Don't download if exists

--- terraform.sh
+++ terraform.sh
@@ -4,12 +4,25 @@ msg() { printf "\033[1;32m :: %s\n\033[0m" "$1"; }
 
 TERRAFORM_VERSION="$1"
 
-msg "Installing terraform $1"
+TERRAFORM_CACHE="${HOME}/.cache/terraform-versions"
+mkdir -p "${TERRAFORM_CACHE}"
 
-wget --quiet --output-document=/tmp/terraform.zip \
-  https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip \
-  && unzip -qo /tmp/terraform.zip -d /usr/local/bin \
-  && rm -f /tmp/terraform.zip \
-  && chmod +x /usr/local/bin/terraform
+cache_miss=$(ls "${TERRAFORM_CACHE}/terraform${TERRAFORM_VERSION}" > /dev/null 2>&1; echo $?)
+
+if [[ ${cache_miss} -eq 1 ]];
+then
+  msg "Downloading terraform $1"
+  mkdir -p /tmp/terraform/
+  wget --quiet --output-document=/tmp/terraform.zip \
+    https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip \
+    && unzip -qo /tmp/terraform.zip -d /tmp/terraform \
+    && mv /tmp/terraform/terraform "${TERRAFORM_CACHE}/terraform${TERRAFORM_VERSION}" \
+    && rm -rf /tmp/terraform.zip /tmp/terraform/ \
+    && chmod +x "${TERRAFORM_CACHE}/terraform${TERRAFORM_VERSION}"
+fi
+
+msg "Switching to terraform $1"
+tf_bin="/usr/local/bin/terraform"
+rm -f ${tf_bin} && ln -s "${TERRAFORM_CACHE}/terraform${TERRAFORM_VERSION}" ${tf_bin}
 
 msg "Done!"
